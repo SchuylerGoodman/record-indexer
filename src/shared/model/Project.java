bfb97f3b9876d8e2b71bb74d1bbde393ad0ba38f
Schuyler Nicholas Goodman <goodm4n@pork.cs.byu.edu>
2013-10-11 16:59:29 -0600

    Started Documentation

--- src/shared/model/Project.java
+++ src/shared/model/Project.java
@@ -18,26 +18,46 @@ public interface Project {
      * 
      * @param project_id the unique id of the given project in the database.
      */
-    public void loadProject(int project_id);
+    public void loadProject(int projectId);
     
     /**
-     * Loads the next batch in line from the database into the project.
+     * Loads the next Batch in line from the database into the project.
      * 
-     * @return next
+     * @return the next Batch in line
      */
     public Batch nextBatch();
     
     /**
-     * Loads the last batch in line from the database into the project.
+     * Loads the last Batch in line from the database into the project.
      * 
-     * @return last
+     * @return the last Batch in line
      */
     public Batch lastBatch();
+    
+    /**
+     * Returns the Batch object that is currently in view.
+     * 
+     * @return the current Batch in view, or the first if just initializing
+     */
+    public Batch currentBatch();
+    
+    /**
+     * Returns the Batch keyed by the batch_id in the database.
+     * 
+     * @param batch_id The unique batch id from the database.
+     * @return a Batch object specified by the batch_id if batch_id belongs to a batch from this project.  Otherwise return <code>null</code>.
+     */
+    public Batch loadBatch(int batchId);
     
     /**
      * Submit the current project and save the input information into the database.
      */
     public void submit();
     
+    @Override
+    public int hashCode();
+    
+    @Override
+    public boolean equals(Object o);
     
 }
