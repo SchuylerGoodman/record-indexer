292d02570cc21010fa63ff25720a205820cb2e81
SchuylerGoodman <schuyler.goodman@gmail.com>
2013-10-14 14:38:18 -0600

    Add search shared.communication classes

---
+++ src/shared/communication/Search_Param.java
@@ -0,0 +1,46 @@
+package shared.communication;
+
+/**
+ * Communication class for the search API
+ * 
+ * @author schuyler
+ */
+public class Search_Param {
+    
+    /**
+     * Getter method for the User's username.
+     * 
+     * @return The username
+     */
+    public String username() {
+        return null;
+    }
+    
+    /**
+     * Getter method for the User's password.
+     * 
+     * @return The password
+     */
+    public String password() {
+        return null;
+    }
+    
+    /**
+     * Getter method for the field ids to search.
+     * 
+     * @return String with comma-separated field ids
+     */
+    public String fields() {
+        return null;
+    }
+    
+    /**
+     * Getter method for the strings to search for.
+     * 
+     * @return String comma-separated list of search strings.
+     */
+    public String values() {
+        return null;
+    }
+    
+}
---
+++ src/shared/communication/Search_Result.java
@@ -0,0 +1,26 @@
+package shared.communication;
+
+import java.util.Collection;
+
+/**
+ * Communication class for the search API
+ * 
+ * @author schuyler
+ */
+public class Search_Result {
+    
+    /**
+     * Getter method for the search results.
+     * 
+     * @return Collection of a Collection of Objects
+     *      Each inner Collection contains:
+     *          Integer batchId
+     *          URL imageURL
+     *          Integer recordNumber
+     *          Integer fieldId
+     */
+    public Collection<Collection<Object>> results() {
+        return null;
+    }
+    
+}
